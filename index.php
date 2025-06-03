13f62f725ead5e543ed06fc660b99864d22552a2
Luan <luanlutii@gmail.com>
2025-06-02 22:23:19 -0300

    fatiando o codigo
    mudanças

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php 
+include "includes/cabecalho.php";
+include "includes/banner.php";
+include "includes/categorias.php"
+?>
+    
+    
+
+    <div class="produtos">
+        <h2 class="titulo">Produtos que estão bombando!</h2>
+        <div class="lista-produtos">
+            <div class="item-produto">
+                <img src="images/camiseta.png" class="foto-produto">
+                <h3 class="titulo-produto">Camiseta Conforto</h3>
+                <p class="descricao-produto">Multicores e tamanhos. Tecido de algodão 100%, fresquinho para o verão. Modelagem unissex.</p>
+                <h3 class="preco-produto">R$ 70,00</h3>
+                <a href="#" class="botao-produto">Ver mais</a>
+            </div>
+            <div class="item-produto">
+                <img src="images/calca.png" class="foto-produto">
+                <h3 class="titulo-produto">Calça Alfaiataria</h3>
+                <p class="descricao-produto">Modelo Wide Leg alfaiataria em linho. Uma peça pra vida toda!</p>
+                <h3 class="preco-produto">R$ 180,00</h3>
+                <a href="#" class="botao-produto">Ver mais</a>
+            </div>
+            <div class="item-produto">
+                <img src="images/tenis.png" class="foto-produto">
+                <h3 class="titulo-produto">Tênis Chunky</h3>
+                <p class="descricao-produto">Snicker casual com solado mais alto e modelagem robusta. Modelo unissex.</p>
+                <h3 class="preco-produto">R$ 250,00</h3>
+                <a href="#" class="botao-produto">Ver mais</a>
+            </div>
+            <div class="item-produto">
+                <img src="images/jaqueta.png" class="foto-produto">
+                <h3 class="titulo-produto">Jaqueta Jeans</h3>
+                <p class="descricao-produto">Modelo unissex oversized com gola de camurça. Atemporal e autêntica!</p>
+                <h3 class="preco-produto">R$ 150,00</h3>
+                <a href="#" class="botao-produto">Ver mais</a>
+            </div>
+            <div class="item-produto">
+                <img src="images/oculos.png" class="foto-produto">
+                <h3 class="titulo-produto">Óculos Redondo</h3>
+                <p class="descricao-produto">Armação metálica em grafite com lentes arredondadas. Sem erro!</p>
+                <h3 class="preco-produto">R$ 120,00</h3>
+                <a href="#" class="botao-produto">Ver mais</a>
+            </div>
+            <div class="item-produto">
+                <img src="images/bolsa.png" class="foto-produto">
+                <h3 class="titulo-produto">Bolsa coringa</h3>
+                <p class="descricao-produto">Bolsa camel em couro sintético de alta duração. Ideal para acompanhar você por uma vida!</p>
+                <h3 class="preco-produto">R$ 120,00</h3>
+                <a href="#" class="botao-produto">Ver mais</a>
+            </div>
+        </div>
+    </div>
+
+    <?php 
+include "includes/facilidades.php"
+?>
+
+ 
+
+    <div class="promocoes">
+        <h3 class="titulo-promocoes">Quer receber nossas novidades, promoções exclusivas e 10% OFF na primeira compra? Cadastre-se!</h3>
+        <form class="formulario-promocoes">
+            <input type="text" class="campo-promocoes" placeholder="Digite seu email">
+            <button type="button" class="botao-promocoes">Enviar</button>
+        </form>
+    </div>
+   
+    <?php 
+include "includes/rodape.php"
+?>
+    <script src="app.js"></script>
+</body>
+</html>
